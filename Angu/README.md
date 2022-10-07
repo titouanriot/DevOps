c503994d9d822cfb85040c30d4a056d80c45ebdd
titouanriot <titouanriot@orange.fr>
2022-10-07 12:06:29 +0200

    verif

--- Angu/README.md
+++ Angu/README.md
@@ -1,4 +1,4 @@
-# DevOpsAngu
+# Angu
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.7.
 
--- Angu/src/app/app.component.spec.ts
+++ Angu/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DevOpsAngu'`, () => {
+  it(`should have as title 'Angu'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('DevOpsAngu');
+    expect(app.title).toEqual('Angu');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('DevOpsAngu app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Angu app is running!');
   });
 });
